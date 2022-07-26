f1bc5ca7f5ae20340f7679d7442ebdec8be8b61e
base-erdepe <110051648+base-erdepe@users.noreply.github.com>
2022-07-27 00:16:57 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -17,5 +17,5 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
 echo Password: #Fikri
-echo Silahkan Login Ke RDP Anda!!
+echo Berhasil DiBuat, Silahkan Login Ke RDP Anda!!
 ping -n 10 127.0.0.1 >nul
